01223c6493b9d85420604eb98ffc5f1be531633f
lnbatioco <LoryleenBatioco@gmail.com>
2018-02-01 15:59:00 -0800

    add teamB methods for button functionality

--- CourtCounter/app/src/main/java/com/loryleen/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/loryleen/courtcounter/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,28 +19,55 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO: TeamA - display the score for Team A
     public void displayScoreTeamA(int score){
-        TextView scoreTeamA = (TextView) findViewById(R.id.teamA_points);
+        TextView scoreTeamA = (TextView) findViewById(R.id.teamA_score);
         scoreTeamA.setText(String.valueOf(score));
     }
 
     // TODO: TeamA - three-pointer
-    public void threePointer(View view){
+    public void threePointerTeamA(View view){
         scoreTeamA = scoreTeamA + 3;
         displayScoreTeamA(scoreTeamA);
-        Toast.makeText(this, "3 points scored!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Team A scored 3 points!", Toast.LENGTH_SHORT).show();
     }
 
     // TODO: TeamA - two-pointer
-    public void twoPointer(View view){
+    public void twoPointerTeamA(View view){
         scoreTeamA = scoreTeamA + 2;
         displayScoreTeamA(scoreTeamA);
-        Toast.makeText(this, "2 points scored!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Team A scored 2 points!", Toast.LENGTH_SHORT).show();
     }
 
     // TODO: TeamA - free throw
-    public void freeThrow(View view){
+    public void freeThrowTeamA(View view){
         scoreTeamA = scoreTeamA + 1;
         displayScoreTeamA(scoreTeamA);
-        Toast.makeText(this, "1 point scored!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Team A scored 1 point!", Toast.LENGTH_SHORT).show();
+    }
+
+    // TODO: TeamB - display the score for Team B
+    public void displayScoreTeamB(int score){
+        TextView scoreTeamB = (TextView) findViewById(R.id.teamB_score);
+        scoreTeamB.setText(String.valueOf(score));
+    }
+
+    // TODO: TeamB - three-pointer
+    public void threePointerTeamB(View view){
+        scoreTeamB = scoreTeamB + 3;
+        displayScoreTeamB(scoreTeamB);
+        Toast.makeText(this, "Team B scored 3 points!", Toast.LENGTH_SHORT).show();
+    }
+
+    // TODO: TeamB - two-pointer
+    public void twoPointerTeamB(View view){
+        scoreTeamB = scoreTeamB + 2;
+        displayScoreTeamB(scoreTeamB);
+        Toast.makeText(this, "Team B scored 2 points!", Toast.LENGTH_SHORT).show();
+    }
+
+    // TODO: TeamB - free throw
+    public void freeThrowTeamB(View view){
+        scoreTeamB = scoreTeamB + 1;
+        displayScoreTeamB(scoreTeamB);
+        Toast.makeText(this, "Team B scored 1 point!", Toast.LENGTH_SHORT).show();
     }
 }
