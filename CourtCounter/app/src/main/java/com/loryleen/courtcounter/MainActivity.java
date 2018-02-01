c19278c98c8dcc9116cb327a20d7315ed8001cf6
lnbatioco <LoryleenBatioco@gmail.com>
2018-02-01 15:44:29 -0800

    add TeamB to xml layout

--- CourtCounter/app/src/main/java/com/loryleen/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/loryleen/courtcounter/MainActivity.java
@@ -8,39 +8,38 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreA = 0;
-    
+    int scoreTeamA = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //displayScoreTeamA(scoreA);
     }
 
-    // TODO: display the score for Team A
+    // TODO: TeamA - display the score for Team A
     public void displayScoreTeamA(int score){
         TextView scoreTeamA = (TextView) findViewById(R.id.teamA_points);
         scoreTeamA.setText(String.valueOf(score));
     }
 
-    // TODO: three-pointer
+    // TODO: TeamA - three-pointer
     public void threePointer(View view){
-        scoreA = scoreA + 3;
-        displayScoreTeamA(scoreA);
+        scoreTeamA = scoreTeamA + 3;
+        displayScoreTeamA(scoreTeamA);
         Toast.makeText(this, "3 points scored!", Toast.LENGTH_SHORT).show();
     }
 
-    // TODO: two-pointer
+    // TODO: TeamA - two-pointer
     public void twoPointer(View view){
-        scoreA = scoreA + 2;
-        displayScoreTeamA(scoreA);
+        scoreTeamA = scoreTeamA + 2;
+        displayScoreTeamA(scoreTeamA);
         Toast.makeText(this, "2 points scored!", Toast.LENGTH_SHORT).show();
     }
 
-    // TODO: free throw
+    // TODO: TeamA - free throw
     public void freeThrow(View view){
-        scoreA = scoreA + 1;
-        displayScoreTeamA(scoreA);
+        scoreTeamA = scoreTeamA + 1;
+        displayScoreTeamA(scoreTeamA);
         Toast.makeText(this, "1 point scored!", Toast.LENGTH_SHORT).show();
     }
 }
